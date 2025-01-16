edec900a7e4ddeaf80501205b132efdaa4cc6377
RobbyR98 <robbyrunge@yahoo.com>
2025-01-16 19:02:12 +0100

    redesign

--- script.js
+++ script.js
@@ -21,17 +21,29 @@ function getFromLocalStorage() {
 function renderAllNotes() {
   getFromLocalStorage();
   renderNotes();
-  renderArchivNotes();
-  renderTrashNotes();
+}
+
+function hideAllSections() {
+  document.getElementById('content').classList.remove('show');
+  document.getElementById('archiv-content').classList.remove('show');
+  document.getElementById('trash-content').classList.remove('show');
 }
 
 function moveNote(indexNote, startKey, destinationKey) {
-  let note = allNotes[startKey].splice(indexNote, 1);
-  allNotes[destinationKey].push(note[0]);
-  let notesTitle = allNotes[startKey + "Titles"].splice(indexNote, 1);
-  allNotes[destinationKey + "Titles"].push(notesTitle[0]);
-  saveToLocalStorage();
-  renderAllNotes();
+  if (allNotes[startKey] && allNotes[destinationKey]) {
+    let note = allNotes[startKey].splice(indexNote, 1)[0];
+    let notesTitle = allNotes[startKey + "Titles"].splice(indexNote, 1)[0];
+    allNotes[destinationKey].push(note);
+    allNotes[destinationKey + "Titles"].push(notesTitle);
+    saveToLocalStorage();
+    if (startKey === "archivNotes") {
+      renderArchivNotes();
+    } else if (startKey === "trashNotes") {
+      renderTrashNotes();
+    } else if (startKey === "notes") {
+      renderNotes();
+    }
+  }
 }
 
 function alertEmptyInput() {
@@ -46,28 +58,34 @@ function alertEmptyInput() {
   }
 }
 
-function renderNotes(){
+function renderNotes() {
+  hideAllSections();
   let contentRef = document.getElementById('content');
   contentRef.innerHTML = "";
   for (let indexNote = 0; indexNote < allNotes.notes.length; indexNote++) {
     contentRef.innerHTML += getNoteTemplate(indexNote);
   }
+  contentRef.classList.add('show');
 }
 
-function renderArchivNotes(){
+function renderArchivNotes() {
+  hideAllSections();
   let archivContentRef = document.getElementById('archiv-content');
   archivContentRef.innerHTML = "";
   for (let indexArchivNote = 0; indexArchivNote < allNotes.archivNotes.length; indexArchivNote++) {
     archivContentRef.innerHTML += getArchivNoteTemplate(indexArchivNote);
   }
+  archivContentRef.classList.add('show');
 }
 
-function renderTrashNotes(){
+function renderTrashNotes() {
+  hideAllSections();
   let trashContentRef = document.getElementById('trash-content');
   trashContentRef.innerHTML = "";
   for (let indexTrashNote = 0; indexTrashNote < allNotes.trashNotes.length; indexTrashNote++) {
     trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
   }
+  trashContentRef.classList.add('show');
 }
 
 function addNote() {  
@@ -81,8 +99,7 @@ function addNote() {
   allNotes.notes.push(noteInput);
   allNotes.notesTitles.push(titleInput);
   saveToLocalStorage();
-  renderAllNotes();
-
+  renderNotes();
   noteInputRef.value = ""; 
   titleInputRef.value = ""; 
 }
@@ -91,5 +108,13 @@ function deleteNote(indexTrashNote) {
   allNotes.trashNotes.splice(indexTrashNote, 1);
   allNotes.trashNotesTitles.splice(indexTrashNote, 1);
   saveToLocalStorage();
-  renderAllNotes();
+  renderTrashNotes();
+}
+
+function setActive(element) {
+  let headings = document.querySelectorAll('aside h2');
+  for (let i = 0; i < headings.length; i++) {
+    headings[i].classList.remove('active');
+  }
+  element.classList.add('active');
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -3,10 +3,10 @@ function getNoteTemplate(indexNote) {
             <p><strong>${allNotes.notesTitles[indexNote]}:</strong><br> ${allNotes.notes[indexNote]}</p>
               <div class="button-position">              
                 <button class="button-style-notes" onclick="moveNote(${indexNote}, 'notes', 'archivNotes')">
-                <img src="./styles/assets/icons/folder-7207892_640.png" alt="" />
+                <img src="./assets/icons/folder-7207892_640.png" alt="" />
                 </button>
                 <button class="button-style-notes" onclick="moveNote(${indexNote}, 'notes', 'trashNotes')">
-                <img src="./styles/assets/icons/rubbish-6664779_640.png" alt="" />
+                <img src="./assets/icons/rubbish-6664779_640.png" alt="" />
                 </button>
               </div>
           </div>`
@@ -17,10 +17,10 @@ function getArchivNoteTemplate(indexArchivNote) {
             <p><strong>${allNotes.archivNotesTitles[indexArchivNote]}:</strong><br> ${allNotes.archivNotes[indexArchivNote]}</p>
               <div class="button-position">              
                 <button class="button-style-notes" onclick="moveNote(${indexArchivNote}, 'archivNotes', 'notes')">
-                <img src="./styles/assets/icons/icon-1970472_640.png" alt="" />
+                <img src="./assets/icons/icon-1970472_640.png" alt="" />
                 </button>
                 <button class="button-style-notes" onclick="moveNote(${indexArchivNote}, 'archivNotes', 'trashNotes')">
-                <img src="./styles/assets/icons/rubbish-6664779_640.png" alt="" />
+                <img src="./assets/icons/rubbish-6664779_640.png" alt="" />
                 </button>
               </div>
           </div>`
@@ -28,13 +28,13 @@ function getArchivNoteTemplate(indexArchivNote) {
 
 function getTrashNoteTemplate(indexTrashNote) {
   return `<div class="notes-container">
-            <p><strong>${allNotes.trashNotesTitles[indexTrashNote]}:</strong><br> ${allNotes.trashNotes[indexTrashNote]}</p>
+            <p><del><strong>${allNotes.trashNotesTitles[indexTrashNote]}:</strong><br> ${allNotes.trashNotes[indexTrashNote]}</del></p>
               <div class="button-position"> 
                 <button class="button-style-notes" onclick="moveNote(${indexTrashNote}, 'trashNotes', 'notes')">
-                <img src="./styles/assets/icons/icon-1970472_640.png" alt="" />
+                <img src="./assets/icons/icon-1970472_640.png" alt="" />
                 </button>
                 <button class="button-style-notes" onclick="deleteNote(${indexTrashNote})">
-                <img src="./styles/assets/icons/rubbish-6664779_640.png" alt="" />
+                <img src="./assets/icons/rubbish-6664779_640.png" alt="" />
                 </button>
               </div>
           </div>`
